2005b653f2be9258118212f3696a42864e538c0d
User <ozodbekdeveloper@gmail.com>
2021-04-02 15:54:06 +0500

    project changed

--- shop/src/components/layout-components/Footer.js
+++ shop/src/components/layout-components/Footer.js
@@ -6,21 +6,96 @@ export default class Footer extends Component {
             <div className="footer-body">
                     <div className="footer">
                         <div className="footer__header">
-                            <h1>Welcome to our site </h1>
-                            <p>Bizning muvafaqqiyatli hamkorlarimiz</p>
+                            <h1 className="footer__header_h1">Welcome to our site </h1>
+                            <p className="footer__header_p">Bizning muvafaqqiyatli hamkorlarimiz</p>
                         </div>
 
                         <div className="footer__date">
+
                             <div className="footer__date_box">
-                                
+                                <div className="footer__date_box--img"></div>
+                                <h2 className="footer__date_box--head">
+                                   Company Name
+                                </h2>
+                                <p className="footer__date_box--about">dfsjd sdfjsjd sdfjsjdfnsdfs sdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                                <ul className="footer__date_box--list">
+                                         <li>Country:Turkey</li>
+                                    <li>Region:izmir</li>
+                                    <li>Street:izmir</li>
+                                    <p>Contact</p>
+                                    <li>Tel:22323123</li>
+                                    <li><i>Telegram:</i></li>
+                                    <li><i></i></li>
+                                    <li><i></i></li>
+                                    <li><i></i></li>
+                                </ul>
                             </div>
-
                             <div className="footer__date_box">
-
+                                <div className="footer__date_box--img"></div>
+                                <h2 className="footer__date_box--head">
+                                   Company Name
+                                </h2>
+                                <p className="footer__date_box--about">dfsjd sdfjsjd sdfjsjdfnsdfs sdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                                <ul className="footer__date_box--list">
+                                         <li>Country:Turkey</li>
+                                    <li>Region:izmir</li>
+                                    <li>Street:izmir</li>
+                                    <p>Contact</p>
+                                    <li>Tel:22323123</li>
+                                    <li><i>Telegram:</i></li>
+                                    <li><i></i></li>
+                                    <li><i></i></li>
+                                    <li><i></i></li>
+                                </ul>
                             </div>
-
                             <div className="footer__date_box">
+                                <div className="footer__date_box--img"></div>
+                                <h2 className="footer__date_box--head">
+                                   Company Name
+                                </h2>
+                                <p className="footer__date_box--about">dfsjd sdfjsjd sdfjsjdfnsdfs sdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                                <ul className="footer__date_box--list">
+                                         <li>Country:Turkey</li>
+                                    <li>Region:izmir</li>
+                                    <li>Street:izmir</li>
+                                    <p>Contact</p>
+                                    <li>Tel:22323123</li>
+                                    <li><i>Telegram:</i></li>
+                                    <li><i></i></li>
+                                    <li><i></i></li>
+                                    <li><i></i></li>
+                                </ul>
+                            </div>
+                            
 
+                        </div>
+                        <div className="footer__end">
+                           
+                            <div className="footer__end_txt">
+                                Agar biz bilan aloqaga chiqmoqchi bo'lsangi quyidagi ijtimoiy tarmoqlar orqali bog'laning yoki qo'ng'iroq qiling:
+                            </div>
+                            <div className="footer__end_smm">
+                                 <div className="footer__end_smm--item">
+                                     <i className="fa fa-phone"></i>
+                                 </div>
+                                 <div className="footer__end_smm--item">
+                                     <i className="fa fa-telegram"></i>
+                                 </div>
+                                 <div className="footer__end_smm--item">
+                                     <i className="fa fa-instagram"></i>
+                                 </div>
+                                 <div className="footer__end_smm--item">
+                                     <i className="fa fa-facebook"></i>
+                                 </div>
+                                 <div className="footer__end_smm--item">
+                                     <i className="fa fa-twitter"></i>
+                                 </div>
+                                 <div className="footer__end_smm--item">
+                                     <i className="fa fa-email"></i>
+                                 </div>
+                            </div>
+                            <div className="footer__end_txt">
+                                Buxoro viloyati 2021<span>&copy;</span>
                             </div>
                         </div>
                         
